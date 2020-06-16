fc39ce7cd7f2f4c35c29b3c4b8f65e2158ee6a85
Kevin Bueno <kean.solano@gmail.com>
2020-06-15 21:53:44 -0500

    Ejemplos de funciones y variables

--- helloWorld.go
+++ helloWorld.go
@@ -1,9 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-// Funcion hola mundo
+// Declaracion de variables
+var x, y, z int = 1, 2, 3
+var c, python, java = true, false, "no!"
+
+// Cosntantes
+const Pi = 3.14
+
+const (
+	Big = 1<<100
+	Small = Big>>99
+)
+
+// Declaracion de funciones
+func add( x, y int ) int {
+	return x + y
+}
+
+// Declaracion struct
+type Vertex struct {
+	X int
+	Y int
+}
 
 func main() {
-	fmt.Println("Hola mundo!")
+
+	// Declaracion de vaiables corta
+	typescript, javascript, kevinscript := true, false, "yes!"
+
+	fmt.Printf("Now you have %g problems.",
+		math.Nextafter(1, 2))
+
+	fmt.Println(add(42, 13))
+
+	fmt.Println(x, y, z, c, python, java, typescript, javascript, kevinscript, Pi)
+
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+		if i < 3 {
+			fmt.Println("IFFFF")
+		}
+	}
+	fmt.Println(sum)
+
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
+
 }
