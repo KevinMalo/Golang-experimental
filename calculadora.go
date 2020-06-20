e74ed544c9cab537eab3b99374747cd142f18ac8
Kevin Bueno <kean.solano@gmail.com>
2020-06-19 21:48:59 -0500

    calculadora terminada

--- calculadora.go
+++ calculadora.go
@@ -8,15 +8,51 @@ import (
 	"strings"
 )
 
-func main() {
+type calc struct {}
+
+func ( calc ) operate( entrada string, operador string ) int {
+	
+	entradaLimpia := strings.Split( entrada, operador )
+
+	operador1 := parsear( entradaLimpia[0] )
+	operador2 := parsear( entradaLimpia[1] )
+	
+	switch operador {
+
+	case "+":
+		fmt.Println(operador1 + operador2)
+		return operador1 + operador2
+	case "-":
+		fmt.Println(operador1 - operador2)
+		return operador1 - operador2
+	case "*":
+		fmt.Println(operador1 * operador2)
+		return operador1 * operador2
+	case "/":
+		fmt.Println(operador1 / operador2)
+		return operador1 / operador2
+	default:
+		fmt.Println(operador, "No esta soportado")
+		return 0
+	}
+}
+
+func parsear( entrada string ) int {
+	operador, _ := strconv.Atoi(entrada)
+	return operador
+}
+
+func leerEntrada() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	operacion := scanner.Text()
-	fmt.Println(operacion)
-	valores := strings.Split(operacion, "+")
-	fmt.Println(valores)
-	fmt.Println(valores[0] + valores[1])
-	operador1, _ := strconv.Atoi(valores[0])
-	operador2, _ := strconv.Atoi(valores[1])
-	fmt.Println(operador1 + operador2,)
+	return scanner.Text()
+}
+
+func main() {
+
+	entrada := leerEntrada()
+	operador := leerEntrada()
+	c := calc{}
+	fmt.Println(c.operate(entrada,operador))
+
 }
