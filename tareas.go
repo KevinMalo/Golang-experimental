720386956180afe7c811cff4315b4e7553df84f3
Kevin Bueno <kean.solano@gmail.com>
2020-06-25 17:59:25 -0500

    Lista de tareas creada

--- tareas.go
+++ tareas.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type taskList struct {
+	tasks []*task
+}
+
+func ( t *taskList ) agregarALista( tl *task ) {
+	t.tasks = append( t.tasks, tl )
+}
+
+func ( t *taskList ) eliminarDeLista( index int) {
+	t.tasks = append( t.tasks[:index], t.tasks[index+1:]... )
+}
+
 type task struct {
 	nombre      string
 	descripcion string
@@ -22,15 +34,36 @@ func (t *task) actualizarNombre(nombre string) {
 
 func main() {
 
-	t := &task{
+	t1 := &task{
 		nombre:      "Completar mi curso de GO",
 		descripcion: "Completar mi curso de GO en platzi en esta semana",
 	}
 
-	fmt.Printf("%+v\n", t)
-	t.marcarCompetla()
-	t.actualizarNombre("Finalizar mi curso de GO")
-	t.actualizarDescripcion("Completar mi curso cuanto antes")
-	fmt.Printf("%+v\n", t)
+	t2 := &task{
+		nombre:      "Completar mi curso de Python",
+		descripcion: "Completar mi curso de Python en platzi en esta semana",
+	}
+
+	t3 := &task{
+		nombre:      "Completar mi curso de C#",
+		descripcion: "Completar mi curso de C# en platzi en esta semana",
+	}
+
+	t4 := &task{
+		nombre:      "Completar mi curso de Node",
+		descripcion: "Completar mi curso de Node en platzi en esta semana",
+	}
+
+	lista := &taskList{
+		tasks: []*task{
+			t1, t2, t3,
+		},
+	}
+
+	fmt.Println( lista.tasks[0] )
+	lista.agregarALista(t4)
+	fmt.Println( len(lista.tasks) )
+	lista.eliminarDeLista(1)
+	fmt.Println( len(lista.tasks) )
 
 }
